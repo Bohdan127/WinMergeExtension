725743b152ce00448ec0cef59357a0b39469a9ee
Bohdan Benetskiy <benetskyybogdan@gmail.com>
2015-09-27 11:00:14 +0200

    First commit

---
+++ WinMergeExtension/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WinMergeExtension
+{
+	class Program
+	{
+		const string DebugIdentifier = "true";
+		const string FilePath = @"C:\WinMergeExtension\StartWinMerge.bat";
+		const string SettingsFile = @"C:\WinMergeExtension\Settings.txt";
+		const string BranchParName = "BrPath";
+		const char SplitterSettings = '=';
+		const char SplitterPath = '\\';
+
+		static bool isDebugMode = false;
+		static string additionalArgs = string.Empty;
+		static string branchesPath = string.Empty;
+		static string inputArgs = string.Empty;
+		static string secondFile = string.Empty;
+		static string curBranch = string.Empty;
+		static List<string> branchNames = new List<string>();
+		static ProcessStartInfo processInfo;
+		static Process process;
+		static StreamReader settings;
+
+		static void Main(string[] args)
+		{
+			if (args.Length >= 1)
+			{
+				CheckMode(args);
+
+				GetSettings();
+
+				if (branchesPath == null || string.Empty.Equals(branchesPath))
+					return;
+
+				inputArgs = args[0] + " ";
+
+				GetBranches();
+
+				Chooser chooseBranch = new Chooser(branchNames);
+
+				chooseBranch.ShowDialog();
+
+				if (chooseBranch.DialogResult == System.Windows.Forms.DialogResult.OK)
+					inputArgs += branchesPath 
+						+ chooseBranch.SelectedElement 
+						+ inputArgs.Remove(0, inputArgs.IndexOf(curBranch) + curBranch.Length) 
+						+ " ";
+				else
+					return;
+
+				StartWinMerge();
+			}
+		}
+
+		private static void StartWinMerge()
+		{
+			processInfo = new ProcessStartInfo(FilePath, inputArgs + additionalArgs);
+			processInfo.CreateNoWindow = true;
+			processInfo.UseShellExecute = false;
+			processInfo.RedirectStandardError = true;
+			processInfo.RedirectStandardOutput = true;
+
+			process = Process.Start(processInfo);
+			process.WaitForExit();
+
+			if (isDebugMode)
+			{
+				string output = process.StandardOutput.ReadToEnd();
+				string error = process.StandardError.ReadToEnd();
+				int exitCode = process.ExitCode;
+
+				MessageBox.Show("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
+				MessageBox.Show("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
+				MessageBox.Show("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
+			}
+
+			process.Close();
+		}
+
+		private static void GetBranches()
+		{
+			if (Directory.GetDirectories(branchesPath) != null)
+				foreach (string path in Directory.GetDirectories(branchesPath))
+				{
+					if (inputArgs.Contains(path))
+						curBranch = path.Split(SplitterPath).Last();
+					else
+						branchNames.Add(path.Split(SplitterPath).Last());
+				}			
+		}
+
+		private static void GetSettings()
+		{
+			settings = new StreamReader(SettingsFile);
+
+			do
+			{
+				string line = settings.ReadLine();
+
+				if (line.StartsWith(BranchParName))
+					branchesPath = line.Split(SplitterSettings)[1];
+			} while (!settings.EndOfStream);
+		}
+
+		private static void CheckMode(string[] args)
+		{
+			foreach (string a in args)
+			{
+				if (DebugIdentifier.Equals(a))
+					isDebugMode = true;
+				inputArgs += a + " ";
+			}
+
+			if (isDebugMode)
+				MessageBox.Show(inputArgs);
+		}
+	}
+}
---
+++ WinMergeExtensionInstaller/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+
+namespace WinMergeExtensionInstaller
+{
+	class Program
+	{
+		const string DirName = @"C:\WinMergeExtension\";
+		const string FilePrName = @"PrepareWinMerge.bat";
+		const string FilePrDebugModeName = @"PrepareWinMergeDebugMode.bat";
+		const string FileStName = @"StartWinMerge.bat";
+		const string FileSettingsName = @"Settings.txt";
+		const string FileExtName = @"WinMergeExtension.exe";
+		const string PrCode = @"start " + DirName + FileExtName + " %1";
+		const string StCode = @"start WinMergeU.exe %3 /maximize %1 %2";
+		const string SentToPath = @"C:\Users\{0}\AppData\Roaming\Microsoft\Windows\SendTo\";
+		
+		static void Main(string[] args)
+		{
+			if(Directory.Exists(DirName))
+				Directory.Delete(DirName,true);
+			Directory.CreateDirectory(DirName);
+
+			StreamWriter outPrFile = new StreamWriter(DirName + FilePrName);
+			outPrFile.WriteLine(PrCode);
+			outPrFile.Close();
+
+			outPrFile = new StreamWriter(DirName + FilePrDebugModeName);
+			outPrFile.WriteLine(PrCode+" true");
+			outPrFile.Close();
+
+			outPrFile = new StreamWriter(DirName + FileStName);
+			outPrFile.WriteLine(StCode);
+			outPrFile.Close();
+
+			outPrFile = new StreamWriter(DirName + FileSettingsName);
+			outPrFile.WriteLine(GetSettingsTemplate());
+			outPrFile.Close();
+
+			File.Copy(DirName + FilePrName, 
+				string.Format(SentToPath, Environment.UserName) + FilePrName, true);
+			File.Copy(DirName + FilePrDebugModeName, 
+				string.Format(SentToPath, Environment.UserName) + FilePrDebugModeName, true);
+			File.Copy(Directory.GetCurrentDirectory()+ "\\" + FileExtName, DirName + FileExtName);
+			
+
+			Environment.Exit(0);
+		}
+
+		private static string GetSettingsTemplate()
+		{
+			StringBuilder res = new StringBuilder();
+			
+			res.AppendLine(new string('*',27));
+			res.AppendLine("WinMergeExtension Settings");
+			res.AppendLine(new string('*',27));
+			res.AppendLine("** Here you can set default location of your branches");
+			res.AppendLine("BrPath=C:\\Sources\\");
+			res.AppendLine(new string('*', 27));
+
+			return res.ToString();
+		}
+	}
+}
