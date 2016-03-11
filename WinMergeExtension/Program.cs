6440307e8cb9e12f99fe71c888f0c567f2b7d605
Bohdan Benetekyi <benetskyybogdan@gmail.com>
2016-03-11 22:07:41 +0100

    It was a log time without committing but with a lot of changes in "offline" I'm was so sorry that here wasn't any commit with versions from 1.0.0.0 to 1.1.0.5 There are was a lot of changes for major releases like 1.0.1.15 (using DevExpress v15.1) Removing it at version 1.0.2.1. Removing WinForm Chooser for WPF Chooser in current 1.1.0.5 release Also already exist keyboard hoocking and doubleclick helpfull functionality

--- WinMergeExtension/Program.cs
+++ WinMergeExtension/Program.cs
@@ -3,78 +3,181 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
+using WpfChooser;
 
 namespace WinMergeExtension
 {
-	class Program
-	{
-		const string DebugIdentifier = "true";
+    class Program
+    {
+        const string DebugIdentifier = "true";
         const string CreateIdentifier = "create";
         const string FilePath = @"C:\WinMergeExtension\StartWinMerge.bat";
-		const string SettingsFile = @"C:\WinMergeExtension\Settings.txt";
-		const string BranchParName = "BrPath";
-		const char SplitterSettings = '=';
-		const char SplitterBytes = ',';
+        const string SettingsFile = @"C:\WinMergeExtension\Settings.txt";
+        const string BranchParName = "BrPath";
+        const char SplitterSettings = '=';
+        const char SplitterVersions = '.';
+        const char SplitterBytes = ',';
         const char SplitterPath = '\\';
         const string BytesVariable = "string WinMergeBytes";
+        const string BytesVariableWpf = "string WpfBytes";
         const string FileExtName = "WinMergeExtension.exe";
+        const string FileWpfExtName = "WpfChooser.exe";
         const string DialogTitle = "Choose WinMergeExtentionInstaller 'Program.cs' file";
         const string Space = " ";
+        const string DefaultUpdatePathName = "UpdateUrl";
+        const string DefaultUpdateFileName = "UpdateFile";
+        const string DefaultUpdatesCheckName = "CheckUpdates";
+        const string DefaultAutoUpdateName = "AutoUpdate";
 
         static bool isDebugMode = false;
-		static string branchesPath = string.Empty;
-		static string inputArgs = string.Empty;
-		static string secondFile = string.Empty;
-		static string curBranch = string.Empty;
+        static bool checkForUpdates = false;
+        static bool automaticallyUpdate = false;
+        static string branchesPath = string.Empty;
+        static string updatePath = string.Empty;
+        static string updateFile = string.Empty;
+        static string inputArgs = string.Empty;
+        static string secondFile = string.Empty;
+        static string curBranch = string.Empty;
         static string bytesLineTemplate = "string WinMergeBytes = \"{0}\";";
+        static string bytesLineTemplateWpf = "string WpfBytes = \"{0}\";";
         static List<string> branchNames = new List<string>();
-		static ProcessStartInfo processInfo;
-		static Process process;
-		static StreamReader settings;
-        
-        [STAThread]
-		static void Main(string[] args)
-		{
+        static ProcessStartInfo processInfo;
+        static Process process;
+        static StreamReader settings;
+
+        [System.STAThreadAttribute()]
+        public static void Main(string[] args)
+        {
             //args[0] = @"C:\Sources\1\1.txt";//for test in VS
             //args[1] = "true";//for test in VS
+
+            CheckMode(args);
+            GetSettings();
+
+            if (checkForUpdates)
+                CheckForUpdates();
+
             if (args.Length >= 1)
-			{
-                if(CreateIdentifier.Equals(args[0]))
+            {
+                if (CreateIdentifier.Equals(args[0]))
                 {
                     CreateInstaller();
                     return;
                 }
 
-				CheckMode(args);
-				GetSettings();
-
                 if (branchesPath == null || string.Empty.Equals(branchesPath))
                 {
                     if (isDebugMode)
-                        MessageBox.Show("BranchesPath is null or empty","Error");
+                        MessageBox.Show("BranchesPath is null or empty", "Error");
                     return;
                 }
 
                 inputArgs = args[0] + Space;
-				GetBranches();
-				Chooser chooseBranch = new Chooser(branchNames);
-				chooseBranch.ShowDialog();
+                GetBranches();
 
-				if (chooseBranch.DialogResult == System.Windows.Forms.DialogResult.OK)
-					inputArgs += branchesPath 
-						+ chooseBranch.SelectedElement 
-						+ inputArgs.Remove(0, inputArgs.IndexOf(curBranch) + curBranch.Length) 
-						+ Space;
-				else
+                MainWindow chooseBranch = new MainWindow();
+                chooseBranch.SetBranches(branchNames);
+                chooseBranch.ShowDialog();
+
+                if (chooseBranch.Selected)
+                    inputArgs += branchesPath
+                        + chooseBranch.SelectedElement
+                        + inputArgs.Remove(0, inputArgs.IndexOf(curBranch) + curBranch.Length)
+                        + Space;
+
+                else
                     return;
 
                 StartWinMerge();
             }
         }
 
+        private static void CheckForUpdates()
+        {
+            try
+            {
+                bool exist = new System.IO.DirectoryInfo(updatePath).Exists;
+                bool shouldUpdate = exist ? ShouldUpdate() : false;
+
+                if (isDebugMode)
+                {
+                    MessageBox.Show(string.Format("{0} - Exists {1}", updatePath, exist));
+                    MessageBox.Show(string.Format("Need Some Update - {0}", shouldUpdate));
+                    MessageBox.Show(string.Format("Automatically Update - {0}", automaticallyUpdate));
+
+                }
+
+                if (exist && shouldUpdate)
+                {
+                    if (!automaticallyUpdate)
+                        shouldUpdate = MessageBox.Show("New Update Available, should update now?"
+                        , "New Version", MessageBoxButtons.OKCancel) == DialogResult.OK;
+                    if (shouldUpdate)
+                    {
+                        if (isDebugMode)
+                        {
+                            MessageBox.Show(string.Format("Call Process.Start({0});", updatePath + updateFile));
+                            MessageBox.Show("Exit for Update");
+                        }
+                        Process.Start(updatePath + updateFile);
+                        Environment.Exit(0);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (isDebugMode)
+                    MessageBox.Show(ex.Message, "Error");
+                return;
+            }
+        }
+
+        private static bool ShouldUpdate()
+        {
+            Assembly remoteAssembly = null;
+            Assembly currAssembly = null;
+
+            remoteAssembly = Assembly.LoadFrom(updatePath + updateFile);
+            currAssembly = Assembly.GetExecutingAssembly();
+
+            string[] remoteAppVersion = remoteAssembly.GetCustomAttributes(false)
+                 .OfType<AssemblyFileVersionAttribute>().Single().Version.Split(SplitterVersions);
+            string[] currAppVersion = currAssembly.GetCustomAttributes(false)
+                .OfType<AssemblyFileVersionAttribute>().Single().Version.Split(SplitterVersions);
+
+            for (int i = 0; i < remoteAppVersion.Count(); i++)
+            {
+                int remAppVer = Convert.ToInt32(remoteAppVersion[i]);
+                int currAppVer = Convert.ToInt32(currAppVersion[i]);
+                if (remAppVer < currAppVer)
+                    break;
+                else if (remAppVer > currAppVer)
+                    return true;
+            }
+
+            return false;
+        }
+
         private static void CreateInstaller()
+        {
+
+
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Title = DialogTitle;
+                dialog.CheckFileExists = true;
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    ReWriteInstaller(dialog.FileName, GetWinMergeBytes());
+                    ReWriteInstallerWpf(dialog.FileName, GetWpfBytes());
+                }
+            }
+        }
+
+        private static string GetWinMergeBytes()
         {
             byte[] buffer = File.ReadAllBytes(Environment.CurrentDirectory + SplitterPath + FileExtName);
             string winMergeBytes = string.Empty;
@@ -82,15 +185,18 @@ namespace WinMergeExtension
             foreach (var b in buffer)
                 winMergeBytes += b + SplitterBytes.ToString();
 
-            winMergeBytes = winMergeBytes.TrimEnd(SplitterBytes);
+            return winMergeBytes.TrimEnd(SplitterBytes);
+        }
 
-            using (OpenFileDialog dialog = new OpenFileDialog())
-            {
-                dialog.Title = DialogTitle;
-                dialog.CheckFileExists = true;
-                if (dialog.ShowDialog() == DialogResult.OK)
-                    ReWriteInstaller(dialog.FileName, winMergeBytes);
-            }
+        private static string GetWpfBytes()
+        {
+            byte[] buffer = File.ReadAllBytes(Environment.CurrentDirectory + SplitterPath + FileWpfExtName);
+            string WpfBytes = string.Empty;
+
+            foreach (var b in buffer)
+                WpfBytes += b + SplitterBytes.ToString();
+
+            return WpfBytes.TrimEnd(SplitterBytes);
         }
 
         private static void ReWriteInstaller(string path, string winMergeBytes)
@@ -112,39 +218,58 @@ namespace WinMergeExtension
             File.WriteAllText(path, content.ToString());
         }
 
+        private static void ReWriteInstallerWpf(string path, string winMergeBytes)
+        {
+            StringBuilder content = new StringBuilder();
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                do
+                {
+                    string line = reader.ReadLine();
+                    if (!line.Contains(BytesVariableWpf))
+                        content.AppendLine(line);
+                    else
+                        content.AppendLine(string.Format(bytesLineTemplateWpf, winMergeBytes));
+                } while (!reader.EndOfStream);
+            }
+
+            File.WriteAllText(path, content.ToString());
+        }
+
         private static void StartWinMerge()
-		{
-			processInfo = new ProcessStartInfo(FilePath, inputArgs);
-			processInfo.CreateNoWindow = true;
-			processInfo.UseShellExecute = false;
-			processInfo.RedirectStandardError = true;
-			processInfo.RedirectStandardOutput = true;
+        {
+            processInfo = new ProcessStartInfo(FilePath, inputArgs);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
 
-			process = Process.Start(processInfo);
-			process.WaitForExit();
+            process = Process.Start(processInfo);
+            process.WaitForExit();
 
-			if (isDebugMode)
-			{
-				string output = process.StandardOutput.ReadToEnd();
-				string error = process.StandardError.ReadToEnd();
-				int exitCode = process.ExitCode;
+            if (isDebugMode)
+            {
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                int exitCode = process.ExitCode;
 
-				MessageBox.Show("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
-				MessageBox.Show("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
-				MessageBox.Show("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
-			}
+                MessageBox.Show("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
+                MessageBox.Show("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
+                MessageBox.Show("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
+            }
 
-			process.Close();
-		}
+            process.Close();
+        }
 
-		private static void GetBranches()
-		{
+        private static void GetBranches()
+        {
             try
             {
                 if (Directory.GetDirectories(branchesPath) != null)
                     foreach (string path in Directory.GetDirectories(branchesPath))
                     {
-                        if (inputArgs.Contains(path))
+                        if (inputArgs.Contains(path + "\\"))
                             curBranch = path.Split(SplitterPath).Last();
                         else
                             branchNames.Add(path.Split(SplitterPath).Last());
@@ -159,7 +284,7 @@ namespace WinMergeExtension
         }
 
         private static void GetSettings()
-		{
+        {
             try
             {
                 settings = new StreamReader(SettingsFile);
@@ -170,27 +295,35 @@ namespace WinMergeExtension
 
                     if (line.StartsWith(BranchParName))
                         branchesPath = line.Split(SplitterSettings)[1];
+                    else if (line.StartsWith(DefaultUpdatePathName))
+                        updatePath = line.Split(SplitterSettings)[1];
+                    else if (line.StartsWith(DefaultUpdateFileName))
+                        updateFile = line.Split(SplitterSettings)[1];
+                    else if (line.StartsWith(DefaultUpdatesCheckName))
+                        checkForUpdates = Convert.ToBoolean(line.Split(SplitterSettings)[1]);
+                    else if (line.StartsWith(DefaultAutoUpdateName))
+                        automaticallyUpdate = Convert.ToBoolean(line.Split(SplitterSettings)[1]);
                 } while (!settings.EndOfStream);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                    if (isDebugMode)
-                        MessageBox.Show(ex.Message, "Error");
+                if (isDebugMode)
+                    MessageBox.Show(ex.Message, "Error");
                 return;
             }
         }
 
-		private static void CheckMode(string[] args)
-		{
-			foreach (string a in args)
-			{
-				if (DebugIdentifier.Equals(a))
-					isDebugMode = true;
-				inputArgs += a + Space;
-			}
+        private static void CheckMode(string[] args)
+        {
+            foreach (string a in args)
+            {
+                if (DebugIdentifier.Equals(a))
+                    isDebugMode = true;
+                inputArgs += a + Space;
+            }
 
-			if (isDebugMode)
-				MessageBox.Show(inputArgs);
-		}
-	}
+            if (isDebugMode)
+                MessageBox.Show(inputArgs);
+        }
+    }
 }
---
+++ WpfChooser/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfChooser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string SelectedElement { get; private set; }
+        public bool Selected { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.bChoose.Click += bCancel_Click;
+            this.bChoose.KeyDown += Form_KeyDown;
+            this.bCancel.Click += bChoose_Click;
+            this.bCancel.KeyDown += Form_KeyDown;
+            this.listBox1.MouseDoubleClick += Form_DoubleClick;
+            this.listBox1.KeyDown += Form_KeyDown;
+            this.KeyDown += Form_KeyDown;
+            this.MouseDoubleClick += Form_DoubleClick;
+            this.listBox1.Focus();
+        }
+
+        public void SetBranches(List<string> branches)
+        {
+            foreach (var item in branches)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        void bChoose_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void bChoose_Click(object sender, System.EventArgs e)
+        {
+            Selected = true;
+            this.Close();
+        }
+
+        void bCancel_Click(object sender, System.EventArgs e)
+        {
+            Selected = false;
+            this.Close();
+        }
+
+        void listBox1_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+                listBox1.SelectedIndex = listBox1.Items.Count > 0 ? 0 : -1;
+            SelectedElement = listBox1.SelectedItem.ToString();
+        }
+
+        private void Form_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                listBox1_SelectedIndexChanged(null, null);
+                bChoose_Click(null, null);
+            }
+            else if (e.Key == System.Windows.Input.Key.Escape)
+            {
+                bCancel_Click(null, null);
+            }
+        }
+
+        private void Form_DoubleClick(object sender, System.EventArgs e)
+        {
+            listBox1_SelectedIndexChanged(null, null);
+            bChoose_Click(null, null);
+        }
+    }
+}
