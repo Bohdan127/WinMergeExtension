169ea81d78c4a63844b1b478c083d9c0835eb90e
Bohdan Benetekyi <benetskyybogdan@gmail.com>
2016-03-17 20:41:31 +0100

    Fix important event bug for buttons OK and Cancel

--- WpfChooser/MainWindow.xaml.cs
+++ WpfChooser/MainWindow.xaml.cs
@@ -25,12 +25,13 @@ namespace WpfChooser
         public MainWindow()
         {
             InitializeComponent();
-            this.bChoose.Click += bCancel_Click;
+            this.bChoose.Click += bChoose_Click;
             this.bChoose.KeyDown += Form_KeyDown;
-            this.bCancel.Click += bChoose_Click;
+            this.bCancel.Click += bCancel_Click;
             this.bCancel.KeyDown += Form_KeyDown;
             this.listBox1.MouseDoubleClick += Form_DoubleClick;
             this.listBox1.KeyDown += Form_KeyDown;
+            this.listBox1.SelectionChanged += listBox1_SelectedIndexChanged;
             this.KeyDown += Form_KeyDown;
             this.MouseDoubleClick += Form_DoubleClick;
             this.listBox1.Focus();
@@ -44,24 +45,19 @@ namespace WpfChooser
             }
         }
 
-        void bChoose_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        void bChoose_Click(object sender, System.EventArgs e)
+        private void bChoose_Click(object sender, System.EventArgs e)
         {
             Selected = true;
             this.Close();
         }
 
-        void bCancel_Click(object sender, System.EventArgs e)
+        private void bCancel_Click(object sender, System.EventArgs e)
         {
             Selected = false;
             this.Close();
         }
 
-        void listBox1_SelectedIndexChanged(object sender, System.EventArgs e)
+        private void listBox1_SelectedIndexChanged(object sender, System.EventArgs e)
         {
             if (listBox1.SelectedItem == null)
                 listBox1.SelectedIndex = listBox1.Items.Count > 0 ? 0 : -1;
